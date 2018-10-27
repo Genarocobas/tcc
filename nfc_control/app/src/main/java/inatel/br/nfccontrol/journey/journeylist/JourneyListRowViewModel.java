55358e06e9ff42bf8bc4d4bb4778fefac4e34eb4
Guilherme Yabu <guilhermeyabu@inatel.br>
2018-10-27 14:00:20 -0300

    (feat) change app icon in each screen

--- nfc_control/app/src/main/java/inatel/br/nfccontrol/journey/journeylist/JourneyListRowViewModel.java
+++ nfc_control/app/src/main/java/inatel/br/nfccontrol/journey/journeylist/JourneyListRowViewModel.java
@@ -1,13 +1,29 @@
 package inatel.br.nfccontrol.journey.journeylist;
 
 import android.databinding.BaseObservable;
+import android.databinding.ObservableField;
 
 import javax.inject.Inject;
 
 public class JourneyListRowViewModel extends BaseObservable {
 
+  public final ObservableField<String> day;
+
+  public final ObservableField<String> enterTimeOne;
+
+  public final ObservableField<String> exitTimeOne;
+
+  public final ObservableField<String> enterTimeTwo;
+
+  public final ObservableField<String> exitTimeTwo;
+
   @Inject
   public JourneyListRowViewModel() {
+    day = new ObservableField<>();
+    enterTimeOne = new ObservableField<>();
+    exitTimeOne = new ObservableField<>();
+    enterTimeTwo = new ObservableField<>();
+    exitTimeTwo = new ObservableField<>();
   }
 
 
