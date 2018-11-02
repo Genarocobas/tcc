7780d5837f9d1ad80f12dc4481a6cb9ee0e93834
Guilherme Yabu <guilhermeyabu@inatel.br>
2018-11-02 16:21:22 -0300

    (feat) send notifications

--- nfc_control/app/src/main/java/inatel/br/nfccontrol/journey/journeylist/JourneyListViewModel.java
+++ nfc_control/app/src/main/java/inatel/br/nfccontrol/journey/journeylist/JourneyListViewModel.java
@@ -1,11 +1,15 @@
 package inatel.br.nfccontrol.journey.journeylist;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.arch.lifecycle.LifecycleOwner;
 import android.content.Context;
+import android.content.Intent;
 import android.databinding.ObservableField;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -18,7 +22,9 @@ import inatel.br.nfccontrol.TccApplication;
 import inatel.br.nfccontrol.account.AccountController;
 import inatel.br.nfccontrol.data.model.Journey;
 import inatel.br.nfccontrol.data.model.User;
+import inatel.br.nfccontrol.service.NotificationPublisher;
 import inatel.br.nfccontrol.utils.Logger;
+import inatel.br.nfccontrol.utils.TimeUtils;
 
 public class JourneyListViewModel {
 
@@ -105,6 +111,9 @@ public class JourneyListViewModel {
     Journey newJourney = new Journey();
     newJourney.setEnterTime1(Calendar.getInstance().getTime());
     mController.insertJourney(newJourney);
+    scheduleNotification(
+        getNotification("Faltam 10 minutos para atingir o limite de jornada continua"),
+        (int) (TimeUtils.MINUTES_IN_MILLIS * 2));
     mJourneyList.add(newJourney);
   }
 
@@ -144,5 +153,28 @@ public class JourneyListViewModel {
   private void setData(List<Journey> journeyList) {
     if (Logger.DEBUG) Log.d(TAG, "setData");
     mJourneyListAdapter.repopulate(journeyList);
+  }
+
+  private void scheduleNotification(Notification notification, int delay) {
+
+    Intent notificationIntent = new Intent(mContext, NotificationPublisher.class);
+    notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, 1);
+    notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
+    PendingIntent pendingIntent = PendingIntent.getBroadcast(mContext, 0, notificationIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT);
+
+    long futureInMillis = SystemClock.elapsedRealtime() + delay;
+    AlarmManager alarmManager = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+    if (alarmManager != null) {
+      alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+    }
+  }
+
+  private Notification getNotification(String content) {
+    Notification.Builder builder = new Notification.Builder(mContext);
+    builder.setContentTitle("Não esqueça de bater seu ponto!");
+    builder.setContentText(content);
+    builder.setSmallIcon(R.drawable.app_icon);
+    return builder.build();
   }
 }
--- nfc_control/app/src/main/java/inatel/br/nfccontrol/utils/TimeUtils.java
+++ nfc_control/app/src/main/java/inatel/br/nfccontrol/utils/TimeUtils.java
@@ -5,20 +5,20 @@ public class TimeUtils {
   public static final String DATEFORMAT = "dd/MM/yyyy";
   public static final String HOURFORMAT = "HH:mm";
 
-  private static long secondsInMilli = 1000;
-  private static long minutesInMilli = secondsInMilli * 60;
-  private static long hoursInMilli = minutesInMilli * 60;
-  private static long daysInMilli = hoursInMilli * 24;
+  public static long SECONDS_IN_MILLIS = 1000;
+  public static long MINUTES_IN_MILLIS = SECONDS_IN_MILLIS * 60;
+  public static long HOURS_IN_MILLIS = MINUTES_IN_MILLIS * 60;
+  public static long DAYS_IN_MILLIS = HOURS_IN_MILLIS * 24;
 
   public static long[] getHour(long time) {
-    long elapsedDays = time / daysInMilli;
-    time = time % daysInMilli;
+    long elapsedDays = time / DAYS_IN_MILLIS;
+    time = time % DAYS_IN_MILLIS;
 
-    long elapsedHours = time / hoursInMilli;
-    time = time % hoursInMilli;
+    long elapsedHours = time / HOURS_IN_MILLIS;
+    time = time % HOURS_IN_MILLIS;
 
-    long elapsedMinutes = time / minutesInMilli;
-    time = time % minutesInMilli;
+    long elapsedMinutes = time / MINUTES_IN_MILLIS;
+    time = time % MINUTES_IN_MILLIS;
 
     return new long[] {elapsedHours, elapsedMinutes};
   }
